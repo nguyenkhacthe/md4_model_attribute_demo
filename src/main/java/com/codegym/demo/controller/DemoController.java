4861e33bf1e7212d24b2544e1a3000190eed9360
the.nguyen <the.nguyen@codegym.vn>
2021-01-19 16:33:06 +0700

    demo model attribute

--- src/main/java/com/codegym/demo/controller/DemoController.java
+++ src/main/java/com/codegym/demo/controller/DemoController.java
@@ -1,14 +1,13 @@
 package com.codegym.demo.controller;
 
+import com.codegym.demo.model.Product;
 import com.codegym.demo.service.NewProductServiceImpl;
 import com.codegym.demo.service.ProductService;
 import com.codegym.demo.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/home")
@@ -17,14 +16,17 @@ public class DemoController {
     ProductService productService = new ProductServiceImpl();
 //    @RequestMapping("/home")
     @GetMapping
-    public String demo() {
-        productService.save();
-        return "product/home";
+    public ModelAndView demo() {
+        ModelAndView modelAndView = new ModelAndView("product/home");
+        modelAndView.addObject("product", new Product(14, "Automated Kalasnikov model 47"));
+        return modelAndView;
     }
 
     @PostMapping
-    public String getValue() {
-        return "redirect:/yasuo";
+    public String getValue(@ModelAttribute Product product)
+    {
+        System.out.println(product);
+        return "redirect:/home/yasuo";
     }
 
     @GetMapping("/yasuo")
--- src/main/webapp/views/product/home.jsp
+++ src/main/webapp/views/product/home.jsp
@@ -14,7 +14,8 @@
 <h1>Goodbye World</h1>
 <h3>What is your last whisper?</h3>
 <form method="post" action="/home">
-    <input type="text" name="lastWhisper" />
+    <input type="text" name="id" value="${product.id}" />
+    <input type="text" name="name" value="${product.name}" />
     <button type="submit">Click</button>
 </form>
 </body>
